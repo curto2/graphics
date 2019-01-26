5936cddce62c6cca76332024c5f404a7485af837
curto2 <curto@estudiants.urv.cat>
2019-01-26 18:21:47 +0100

    Update Graphics

--- Scripts_Tensorflow/classification.py
+++ Scripts_Tensorflow/classification.py
@@ -1,5 +1,5 @@
 
-# Original python notebook from Fridman fridman@mit.edu
+# Original python notebook from Fridman <fridman@mit.edu>
 # Slightly modified by Curtó and Zarza
 # {curto,zarza}@estudiants.urv.cat
 
@@ -9,74 +9,74 @@ import sys, os, time
 import itertools
 import math, random
 import glob
-import tensorflow as tf
+import tensorflow as tw
 import numpy as np
 import cv2
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-from IPython.display import Image, display
 import warnings
 warnings.filterwarnings('ignore')
 
 # Basic parameters
 
 m_epochs = 22
-base_image_path = "gender/" # Adapt for specific class, generate folder with subfolders for each attribute
-image_types = ["male","female"] # Add specific attributes
-input_img_x = 32
-input_img_y = 32
+path_class = "gender/" # Adapt for specific class, generate folder with subfolders for each attribute
+attributes = ["male","female"] # Add specific attributes
+sample_x = 32
+sample_y = 32
 train_test_split_ratio = 0.9
 batch_size = 32
 checkpoint_name = "output/cz.ckpt"
 
-
 # Helper layer functions
+
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+    initial = tw.truncated_normal(shape, stddev=0.1)
+    return tw.Variable(initial)
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+    initial = tw.constant(0.1, shape=shape)
+    return tw.Variable(initial)
 
 def ctn2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='SAME')
+    return tw.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='SAME')
 
 def m_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+    return tw.nn.max_pool(x, ksize=[1, 2, 2, 1],
                         strides=[1, 2, 2, 1], padding='SAME')
 
 # Model
 
-x = tf.placeholder(tf.float32, shape=[None, input_img_x, input_img_y, 3])
-y_ = tf.placeholder(tf.float32, shape=[None, len(image_types)])
+x = tw.placeholder(tw.float32, shape=[None, sample_x, sample_y, 3])
+y_ = tw.placeholder(tw.float32, shape=[None, len(attributes)])
 
-x_image = x
+x_2 = x
 
 # Our first six convolutional layers of 16 3x3 filters
+
 W_ctn1 = weight_variable([3, 3, 3, 16])
 b_ctn1 = bias_variable([16])
-h_ctn1 = tf.nn.relu(ctn2d(x_image, W_ctn1, 1) + b_ctn1)
+h_ctn1 = tw.nn.relu(ctn2d(x_2, W_ctn1, 1) + b_ctn1)
 
 W_ctn2 = weight_variable([3, 3, 16, 16])
 b_ctn2 = bias_variable([16])
-h_ctn2 = tf.nn.relu(ctn2d(h_ctn1, W_ctn2, 1) + b_ctn2)
+h_ctn2 = tw.nn.relu(ctn2d(h_ctn1, W_ctn2, 1) + b_ctn2)
 
 W_ctn3 = weight_variable([3, 3, 16, 16])
 b_ctn3 = bias_variable([16])
-h_ctn3 = tf.nn.relu(ctn2d(h_ctn2, W_ctn3, 1) + b_ctn3)
+h_ctn3 = tw.nn.relu(ctn2d(h_ctn2, W_ctn3, 1) + b_ctn3)
 
 W_ctn4 = weight_variable([3, 3, 16, 16])
 b_ctn4 = bias_variable([16])
-h_ctn4 = tf.nn.relu(ctn2d(h_ctn3, W_ctn4, 1) + b_ctn4)
+h_ctn4 = tw.nn.relu(ctn2d(h_ctn3, W_ctn4, 1) + b_ctn4)
 
 W_ctn5 = weight_variable([3, 3, 16, 16])
 b_ctn5 = bias_variable([16])
-h_ctn5 = tf.nn.relu(ctn2d(h_ctn4, W_ctn5, 1) + b_ctn5)
+h_ctn5 = tw.nn.relu(ctn2d(h_ctn4, W_ctn5, 1) + b_ctn5)
 
 W_ctn6 = weight_variable([3, 3, 16, 16])
 b_ctn6 = bias_variable([16])
-h_ctn6 = tf.nn.relu(ctn2d(h_ctn5, W_ctn6, 1) + b_ctn6)
+h_ctn6 = tw.nn.relu(ctn2d(h_ctn5, W_ctn6, 1) + b_ctn6)
 
 # Our pooling layer
 
@@ -89,19 +89,19 @@ b_fc1 = bias_variable([2])
 
 # We flatten our pool layer into a fully connected layer
 
-h_pool4_flat = tf.reshape(h_pool4, [-1, n2*n3*n4])
+h_pool4_flat = tw.reshape(h_pool4, [-1, n2*n3*n4])
 
-y = tf.matmul(h_pool4_flat, W_fc1) + b_fc1
+y = tw.matmul(h_pool4_flat, W_fc1) + b_fc1
 
-sn = tf.InteractiveSession()
+sn = tw.InteractiveSession()
 
 # Our loss function and optimizer
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y_, logits = y))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-sn.run(tf.initialize_all_variables())
+loss = tw.reduce_mean(tw.nn.softmax_cross_entropy_with_logits(labels = y_, logits = y))
+train_step = tw.train.AdamOptimizer(1e-4).minimize(loss)
+sn.run(tw.initialize_all_variables())
 
-saver = tf.train.Saver()
+saver = tw.train.Saver()
 time_start = time.time()
 
 v_loss = least_loss = 99999999
@@ -110,21 +110,20 @@ v_loss = least_loss = 99999999
 
 full_set = []
 
-for img_type in image_types:
-    for ex in glob.glob(os.path.join(base_image_path, img_type, "*")):
-        img = cv2.imread(ex)
-        if not img is None:
-            img = cv2.resize(img, (32, 32))
+for abts in attributes:
+    for ex in glob.glob(os.path.join(path_class, abts, "*")):
+        sample = cv2.imread(ex)
+        if not sample is None:
+            sample = cv2.resize(sample, (32, 32))
 
             # Create an array representing our classes and set it
-            one_hot_array = [0] * len(image_types)
-            one_hot_array[image_types.index(img_type)] = 1
-            assert(img.shape == (32, 32, 3))
+            one_hot_array = [0] * len(attributes)
+            one_hot_array[attributes.index(abts)] = 1
+            assert(sample.shape == (32, 32, 3))
 
-            full_set.append((img, one_hot_array, ex))
+            full_set.append((sample, one_hot_array, ex))
 
 random.shuffle(full_set)
-
 
 # We split our data into a training and test set here
 
@@ -133,6 +132,7 @@ train_set = full_set[:split_index]
 test_set = full_set[split_index:]
 
 # We ensure that our training and test sets are a multiple of batch size
+
 train_set_offset = len(train_set) % batch_size
 test_set_offset = len(test_set) % batch_size
 train_set = train_set[: len(train_set) - train_set_offset]
@@ -150,6 +150,7 @@ vn_loss = []
 for z in range(0, m_epochs):
 
     # Iterate over our training set
+
     for tt in range(0, (len(train_x) // batch_size)):
         start_batch = batch_size * tt
         end_batch = batch_size * (tt + 1)
@@ -177,8 +178,6 @@ for z in range(0, m_epochs):
 
     sys.stdout.write("\n")
 
-
-
 plt.figure()
 plt.xticks(np.arange(0, len(train_loss), 1.0))
 plt.ylabel("Loss")
@@ -193,22 +192,22 @@ cfn_true = []
 cfn_ptd = []
 for tt in range(0, len(zipped_x_y)):
     q = zipped_x_y[tt]
-    sfmax = list(sn.run(tf.nn.softmax(y.eval(feed_dict={x: [q[0]]})))[0])
+    sfmax = list(sn.run(tw.nn.softmax(y.eval(feed_dict={x: [q[0]]})))[0])
     sf_idx = sfmax.index(max(sfmax))
     
-    predicted_label = image_types[sf_idx]
-    actual_label = image_types[q[1].index(max(q[1]))]
+    predicted_label = attributes[sf_idx]
+    actual_label = attributes[q[1].index(max(q[1]))]
     
     cfn_true.append(actual_label)
     cfn_ptd.append(predicted_label)
     
     if predicted_label != actual_label:
         print("Actual: {}, predicted: {}".format(actual_label, predicted_label))
-        img_path = test_z[tt]    
-        epl_img = cv2.imread(filename=img_path)
-
+        path_sample = test_z[tt]    
+        epl_sample = cv2.imread(filename=path_sample)
 
 # From sklearn docs
+
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
@@ -240,7 +239,7 @@ def plot_confusion_matrix(cm, classes,
 
 cnf_matrix = confusion_matrix(cfn_true, cfn_ptd)
 plt.figure()
-plot_confusion_matrix(cnf_matrix, classes=image_types, normalize=False,
+plot_confusion_matrix(cnf_matrix, classes=attributes, normalize=False,
                       title='Normalized confusion matrix')
 plt.show()
 
